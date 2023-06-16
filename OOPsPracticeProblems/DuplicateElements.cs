6bf1239aa3e5902edf9cbdfc3f97343eb43a1504
rishiraj007 <rishiraj@mounttalent.com>
2023-06-16 19:09:40 +0530

    [Prabhu]Add: find duplicate element in array

---
+++ OOPsPracticeProblems/DuplicateElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsPracticeProblems
+{
+    internal class DuplicateElements
+    {
+        public int CountDuplicateElements(int[] array)
+        {
+            int count = 0;
+            Dictionary<int, int> elementCount = new Dictionary<int, int>();
+
+            // Count the occurrences of each element in the array
+            foreach (int element in array)
+            {
+                if (elementCount.ContainsKey(element))
+                    elementCount[element]++;
+                else
+                    elementCount[element] = 1;
+            }
+
+            // Count the duplicate elements
+            foreach (int value in elementCount.Values)
+            {
+                if (value > 1)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
--- OOPsPracticeProblems/Program.cs
+++ OOPsPracticeProblems/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("OOPs Practice Problems!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect option \n1.Count Duplicate Elements in array \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        DuplicateElements duplicateElements = new DuplicateElements();
+                        int[] arr = { 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9 };
+                        int duplicateCount = duplicateElements.CountDuplicateElements(arr);
+                        Console.WriteLine("Total number of duplicate elements: " + duplicateCount);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
