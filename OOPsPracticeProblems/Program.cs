042e2b8d4ca55bc33e519b9f98314ba5086c642e
rishiraj007 <rishiraj@mounttalent.com>
2023-06-16 19:00:55 +0530

    [Prabhu]Add: OOPs Practice Problems

---
+++ OOPsPracticeProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPsPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOPs Practice Problems!");
+        }
+    }
+}
